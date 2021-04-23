0c0de395cc1cb14223fbc548b350e10e42190249
Maxsuel Santos <santosmaxsuel17@gmail.com>
2021-04-23 10:58:07 -0300

    Initial commit

---
+++ src/main/java/br/com/max/addresmanager/AddresmanagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.max.addresmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddresmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddresmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/max/addresmanager/AddresmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.max.addresmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddresmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
